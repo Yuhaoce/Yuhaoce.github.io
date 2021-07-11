7cee13968a0ad198aeb61edbceb9bcfc01d92e94
Yuhaoce <73988710+Yuhaoce@users.noreply.github.com>
2021-07-11 15:46:06 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {    
+        setTimeout(() => {
+                // $(".loading-div").hide();
+                // $('body').css('overflow-y','scroll');
+           var demo=  document.getElementsByClassName("loading-div")
+              demo[0].className="attribute-changes"
+             }, 2000);
+    }
+  }
