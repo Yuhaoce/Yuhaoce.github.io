3432b7de7e33178097f4099d8699d9f0744b89e3
Yuhaoce <73988710+Yuhaoce@users.noreply.github.com>
2021-07-12 13:05:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {    
-        setTimeout(() => {
            var demo=  document.getElementsByClassName("loading-div")
               demo[0].className="attribute-changes"
            document.querySelector('body').style.overflowY='scroll'
-             }, 1000);
     }
   }
